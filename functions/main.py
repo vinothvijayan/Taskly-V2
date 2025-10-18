d53745d52723244dc749bcad41cd145f1cd9d1bc
[dyad] <vinoth@betelgeusetech.com>
2025-10-18 14:31:21 +0530

    [dyad] Removing Meetly functions - wrote 1 file(s), deleted 1 file(s) + extra files edited outside of Dyad

--- functions/main.py
+++ functions/main.py
@@ -3,9 +3,6 @@ import os
 
 sys.path.append(os.path.dirname(__file__))
 
-# Import functions from your meetly file
-from process_audio import process_audio_recording, reprocess_recording, get_recording_status
-
 # Import the new function from your journal file
 from process_journal import process_journal_entry, exportJournalToPdf
 
@@ -14,9 +11,6 @@ from sales_tools import get_google_business_data
 
 # Export ALL functions so Firebase can discover them
 __all__ = [
-    'process_audio_recording',
-    'reprocess_recording',
-    'get_recording_status',
     'process_journal_entry',
     'exportJournalToPdf',
     'get_google_business_data',
