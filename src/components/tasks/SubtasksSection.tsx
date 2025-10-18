30765f440ac02f9d8aefbb59db927fa2c82b4206
[dyad] <vinoth@betelgeusetech.com>
2025-10-19 00:33:52 +0530

    [dyad] Removing focus outline from subtask input - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/tasks/SubtasksSection.tsx
+++ src/components/tasks/SubtasksSection.tsx
@@ -190,7 +190,7 @@ export function SubtasksSection({ task }: SubtasksSectionProps) {
                   onChange={(e) => setNewSubtaskTitle(e.target.value)}
                   onKeyPress={handleKeyPress}
                   onClick={(e) => e.stopPropagation()}
-                  className="flex-1 h-9 focus-visible:ring-1 focus-visible:ring-offset-0"
+                  className="flex-1 h-9 focus-visible:ring-0 focus-visible:ring-offset-0"
                 />
                 <Button size="icon" onClick={handleAddSubtask} disabled={!newSubtaskTitle.trim()} className="h-9 w-9">
                   <Plus className="h-4 w-4" />
