64ff7299b820d49bbedf238da04120041fb9e48b
[dyad] <vinoth@betelgeusetech.com>
2025-10-18 16:55:48 +0530

    [dyad] Cleaning up old cloud functions - wrote 1 file(s) + extra files edited outside of Dyad

--- functions/main.py
+++ functions/main.py
@@ -1,17 +1,19 @@
 import sys
 import os
 
+# Ensure the current directory is in the path
 sys.path.append(os.path.dirname(__file__))
 
 # Import functions from their respective files
 from process_journal import process_journal_entry, exportJournalToPdf
 from sales_tools import get_google_business_data
-from meetly_processor import process_meetly_recording # New import
+from meetly_processor import process_meetly_recording
 
-# Export ALL functions so Firebase can discover them
+# Export ALL functions so Firebase can discover them.
+# This list is the single source of truth for what functions should be deployed.
 __all__ = [
     'process_journal_entry',
     'exportJournalToPdf',
     'get_google_business_data',
-    'process_meetly_recording', # New export
+    'process_meetly_recording',
 ]
