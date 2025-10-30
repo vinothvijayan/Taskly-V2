fb370339c06e245b3e2a5ec18af3e2770f91689a
[dyad] <vinoth@betelgeusetech.com>
2025-10-30 15:04:59 +0530

    [dyad] wrote 1 file(s)

--- src/components/common/CustomReminderDialog.tsx
+++ src/components/common/CustomReminderDialog.tsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogTrigger } from '@/components/ui/dialog';
 import { Badge } from '@/components/ui/badge';
 import { TimePickerSelect } from '@/components/ui/time-picker';
 
